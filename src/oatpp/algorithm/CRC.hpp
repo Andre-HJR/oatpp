4d16d5507d464d66496cf8523606ca4f3a01f82b
Andre_HJR <44284560+Andre-HJR@users.noreply.github.com>
2023-06-08 22:32:28 +0800

    move crc32policy to policy/algorithm.

--- src/oatpp/algorithm/CRC.hpp
+++ src/oatpp/algorithm/CRC.hpp
@@ -28,6 +28,9 @@
 #include "oatpp/core/base/Environment.hpp"
 
 #include "oatpp/encoding/Hex.hpp"
+#include "oatpp/core/utils/policy/algorithm/CRC32Policy.hpp"
+
+
 
 namespace oatpp { namespace algorithm {
 
@@ -43,44 +46,19 @@ public:
   static const p_uint32 TABLE_04C11DB7;
 public:
 
-  template <typename Policy = CRC32Policy>
+  template <typename Policy = ::oatpp::utils::policy::algorithm::CRC32Policy>
   static v_uint32 bitReverse(v_uint32 poly);
   
   /**
    *  Generates v_uint32 table[256] for polynomial
    */
-  template <typename Policy = CRC32Policy>
+  template <typename Policy = ::oatpp::utils::policy::algorithm::CRC32Policy>
   static p_uint32 generateTable(v_uint32 poly);
 
-  template <typename Policy = CRC32Policy>
+  template <typename Policy = ::oatpp::utils::policy::algorithm::CRC32Policy>
   static v_uint32 calc(const void *buffer, v_buff_size size, v_uint32 crc = 0, v_uint32 initValue = 0xFFFFFFFF, v_uint32 xorOut = 0xFFFFFFFF, p_uint32 table = TABLE_04C11DB7);
   
 };
-/**
- * Implementation of CRC-32 Policy. Cyclic redundancy check algorithm.
- */
-
-struct CRC32Policy {
-  static v_uint32 bitReverse(v_uint32 poly);
-  
-  /**
-   *  Generates v_uint32 table[256] for polynomial
-   */
-  static p_uint32 generateTable(v_uint32 poly);
-
-  /**
-   * Calculate CRC32 value for buffer of defined size
-   * @param buffer
-   * @param size
-   * @param crc
-   * @param initValue
-   * @param xorOut
-   * @param table
-   * @return - CRC32 value (v_uint32)
-   */
-  static v_uint32 calc(const void* buffer, v_buff_size size, v_uint32    crc , v_uint32    initValue, v_uint32    xorOut  , p_uint32    table   );
-};
-    
 }}
 
 #endif /* oatpp_algorithm_CRC_hpp */
